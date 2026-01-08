5d44277ccb54d514b8989faa6a0d8b49ede5f3e2
Vicwin13 <vnwimo13@gmail.com>
2026-01-08 13:51:29 +0100

    authentication

--- lendsqr/src/components/ProtectedRoute.tsx
+++ lendsqr/src/components/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import { useRouter } from "next/navigation";
 
 interface Props {
@@ -9,14 +10,22 @@ interface Props {
 
 export default function ProtectedRoute({ children }: Props) {
   const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
   useEffect(() => {
     const isAuth = localStorage.getItem("isAuthenticated");
 
     if (!isAuth) {
       router.replace("/login");
+    } else {
+      setIsAuthenticated(true);
     }
   }, [router]);
+
+  // Don't render anything while checking authentication or if not authenticated
+  if (isAuthenticated === null || isAuthenticated === false) {
+    return null;
+  }
 
   return <>{children}</>;
 }
